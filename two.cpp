54d9bbfc3ea8979d7b8aa1b1926ec88b700616c3
Lampson <178179905+swamp-chan@users.noreply.github.com>
2025-09-13 21:49:11 +0545

    did sum in two.cpp ;3

--- two.cpp
+++ two.cpp
@@ -12,16 +12,60 @@ void menu();
 
 int main(){
 	int option;
-	do{
-		cout<<"Pick which program you want to run: "<<endl;
-		cout<<"1. Geometric sequence";
-		cout<<"2. Verifying age";
-		cout<<"3. Infinite loop until 0 is entered";
-		cout<<"4. Number guessing \"game\"";
-		cout<<"5. Corresponding day";
-	}while(option<5 && option>0);
+	do
+	{
+		system("cls");
+		menu();
+		cin >> option;
+		system("cls");
+		switch (option){
+			case 1:	
+				geometricsequence();
+			break;
+			case 2:
+				checkage();
+			break;
+			case 3:
+				infinite();
+			break;
+			case 4:
+				numberguessinggame();
+			break;
+			case 5:
+				correspondingday();
+			break;
+		}
+	}while(option<=5 && option>0);
 	cin.get();
 	return 0;
+	
 }
+
+void menu(){
+	cout<<"Pick which program you want to run: "<<endl;
+	cout<<"1. Geometric sequence"<<endl;
+	cout<<"2. Verifying age"<<endl;
+	cout<<"3. Infinite loop until 0 is entered"<<endl;
+	cout<<"4. Number guessing \"game\""<<endl;
+	cout<<"5. Corresponding day"<<endl;
+	cout<<"Any key to exit."<<endl;
+	cout<<endl;
+}
+
+void geometricsequence(){
+	int first_term, ratio, n_term;
+	cout<<"geometric sequence"<<endl;
+	cout<<"\nenter first term, ratio and no. of terms(a, r, n): "<<endl;
+	cin >>  first_term >>  ratio >>  n_term;
+	for(int i = 0; i<n_term; i++){
+		cout << first_term << endl;
+		first_term *= ratio;
+	}
+	system("pause");
+}
+void checkage(){}
+void infinite(){}
+void numberguessinggame(){}
+void correspondingday(){}
 
 
